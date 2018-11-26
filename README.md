0dabb777e108f8178aef6b26d165881beeccdba9
vbbphc <balloli.vb@gmail.com>
2018-11-27 01:37:48 +0530

    Adds: Mouse and Keyboard inputs

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # Bezier Curve
 
-Bezier curve is a curve with 4 control points. This project aims to implement a
+Bezier curve is a curve of degree n with n+1 control points. This project aims to implement a bezier curve using de casteljau's algorithm in OpenGL.
 
-bezier curve using de casteljau's algorithm in OpenGL.
+## Algorithm
+
+Lerp is recursively applied to the control points until it converges to a single point.
+
+Addition of point: Add's x,y to the 2D control array.
+
+Deletion of point: Removes x,y from the 2D control array within a TOUCH_BUFFER range.
+
+Dragging a point: Stores previous touch and current point on lift off within the TOUCH_BUFFER and changes the point.
+
+t is controlled using RES parameter in the code.
 
 ## Run instructions
 
 `gcc bezier.c -lLG -lGLU -lglut`
+
+### Keyboard shortcuts:
+
+`a: adds point on the cuurve
+ d: delete's point on the curve
+ b: draggable points on the curve`
+
+### Mouse shortcuts
+
+`Left click only`
--- bezier.c
+++ bezier.c
@@ -1,17 +1,93 @@
 #include <GL/glut.h>
 #include <stdio.h>
 
-float control_points[4][2] = {
-  {100, 100},
-  {100, 200},
-  {200, 200},
-  {200, 100}
+float control_points[100][2] = {
+
 };
 int points_count = 0;
 
 int DIMEN_X = 600;
 int DIMEN_Y = 600;
 
+int STATE = -1;
+int ADD_STATE = 0;
+int DELETE_STATE = 1;
+int DRAG_STATE = 2;
+
+float TOUCH_BUFFER = 4;
+int RES = 1000; // Pixel resolution
+
+float drag_x, drag_y;
+
+/*! \brief Removes coordinates from control points array.
+*
+*
+*   \param x: x-coordinate to be removed.
+*   \param y: y-coordinate to be removed.
+*/
+void remove_coordinates(float x, float y)
+{
+  int temp_count = points_count;
+  for (int i = 0; i <= temp_count; i++)
+  {
+    if (control_points[i][0] - x < TOUCH_BUFFER && control_points[i][0] - x > -1 * TOUCH_BUFFER)
+    {
+      if (control_points[i][1] - y < TOUCH_BUFFER && control_points[i][1] - y > -1 * TOUCH_BUFFER)
+      {
+        control_points[i][0] = control_points[i+1][0];
+        control_points[i][1] = control_points[i+1][1];
+        temp_count--;
+      }
+    }
+  }
+  points_count = temp_count;
+}
+
+/*! \brief Adds coordinates to the control points array.
+*
+*
+*   \param x: x-coordinate to be added.
+*   \param y: y-coordinate to be added.
+*/
+void add_coordinates(float x, float y)
+{
+  control_points[points_count][0] = x;
+  control_points[points_count][1] = y;
+  points_count++;
+}
+
+/*! \brief Replaces a control point in the array.
+*
+*
+*   \param x_old: x-coordinate to be replaced.
+*   \param y_old: y-coordinate to be replaced.
+*   \param x_new: new x-coordinate
+*   \param y_new: new y-coordinate
+*/
+void replace_coordinates(float x_old, float y_old, float x_new, float y_new)
+{
+  for (int i = 0; i < points_count; i++)
+  {
+
+    if (control_points[i][0] - x_old < TOUCH_BUFFER && control_points[i][0] - x_old > -1 * TOUCH_BUFFER)
+    {
+      if (control_points[i][1] - y_old < TOUCH_BUFFER && control_points[i][1] - y_old > -1 * TOUCH_BUFFER)
+      {
+        control_points[i][0] = x_new;
+        control_points[i][1] = y_new;
+        break;
+      }
+    }
+  }
+}
+
+/*! \brief Plots point on the window.
+*
+*
+*   \param x: x-coordinate to be plotted.
+*   \param y: y-coordinate to be plotted.
+*   \param color: r value in the rgb scheme.
+*/
 void plotPoint (int x, int y, float color)
 {
   glBegin(GL_POINTS);
@@ -20,18 +96,30 @@ void plotPoint (int x, int y, float color)
   glEnd();
 }
 
+/*! \brief Point on the line segment between 2 points.
+*
+*
+*   \param start: 2 dimensional start vertex.
+*   \param end: 2 dimensional end vertex.
+*   \param t: float value - point on the line segment between the vertices.
+*/
 float * lerp (float start[2], float end[2], float t)
 {
   static float req[2];
   req[0] =  start[0] + t * (end[0] - start[0]);
   req[1] = start[1] + t * (end[1] - start[1]);
-  printf("X0: %f  X1: %f \t Y0: %f  Y1: %f \t t: %f   OUTX: %f   OUTY: %f \n",start[0] ,end[0], start[1], end[1], t, req[0], req[1]);
   return req;
 }
 
+/*! \brief Constructs a bezier curve recursively using deCasteljau's algorithm.
+*
+*
+*   \param controls: 2 column matrix of vertices recursively lerped.
+*   \param number: number of coordinates
+*   \param t: distance on the line segment between two vertices
+*/
 void bezier (float controls[][2], int number, float t)
 {
-  //printf("Number: %d\n", number);
   float control_outputs[number-1][2];
   for (int i = 0; i < number - 1; i++)
   {
@@ -48,38 +136,63 @@ void bezier (float controls[][2], int number, float t)
   }
 }
 
-void test ()
-{
-  float s[2] = {100, 100};
-  float s1[2] = {100, 200};
-  lerp(s, s1, 0.5);
-}
-
+/*! \brief OpenGL mouse callback.
+*
+*
+*   \param button: Button clicked on the mouse.
+*   \param state : State of the button clicked.
+*   \param x,y: Inverted coordinates on the window
+*/
 void mouse(int button, int state, int x, int y)
 {
-  // printf("x:%d \t y:%d \n", x,y);
-  // 	if (button == GLUT_LEFT_BUTTON)
-  // 	{
-	// 	if (state == GLUT_DOWN) {
-  //     int i = 0;
-  //     for (i=0; i<points_count; i++)
-  //     {
-  //       if (control_points[i][0] == x && control_points[i][1] == DIMEN_Y-y)
-  //       {
-  //
-  //       }
-  //     }
-	// 	  a = x;
-	// 	  b = DIMEN_Y - y;
-	// 	}
-	// 	if (state == GLUT_UP) {
-  //     a = x;
-  //     b = DIMEN_Y - y;
-  //     glutPostRedisplay();
-	// 	}
-  // 	}
+  y = DIMEN_Y - y;
+  	if (button == GLUT_LEFT_BUTTON)
+  	{
+		if (state == GLUT_DOWN) {
+      if (STATE == ADD_STATE)
+      {
+        add_coordinates(x, y);
+      } else if (STATE == DELETE_STATE)
+      {
+        remove_coordinates(x,y);
+      } else if (STATE == DRAG_STATE)
+      {
+        drag_x = x;
+        drag_y = y;
+      }
+		}
+		if (state == GLUT_UP && STATE == DRAG_STATE) {
+      replace_coordinates(drag_x, drag_y, x, y);
+      glutPostRedisplay();
+		}
+  	}
+    glutPostRedisplay();
 }
 
+/*! \brief OpenGL keyboard callback.
+*
+*
+*   \param key: Key on the keyboard.
+*/
+void keyboard(unsigned char key, int x, int y)
+{
+  switch (key) {
+    case 'a':  // Set's state to ADD
+      STATE = ADD_STATE;
+      break;
+    case 'd': // Set's state to DELETE
+      STATE = DELETE_STATE;
+      break;
+    case 'b': // Set's state to DRAG
+      STATE = DRAG_STATE;
+      break;
+  }
+}
+
+/*! \brief Initializes OpenGL parameters.
+*
+*
+*/
 void init()
 {
   glClearColor(0.3, 0.2, 1.0, 0.1);
@@ -88,33 +201,42 @@ void init()
   glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
   glPointSize(10.0f);
-
 }
 
+/*! \brief OpenGL display callback. Runs the bezier curve implementation.
+*
+*/
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT);
-  plotPoint((int)control_points[0][0], (int)control_points[0][1], 1.0);
-  plotPoint((int)control_points[1][0], (int)control_points[1][1], 1.0);
-  plotPoint((int)control_points[2][0], (int)control_points[2][1], 1.0);
-  plotPoint((int)control_points[3][0], (int)control_points[3][1], 1.0);
   int i = 0;
-  for (i=0; i<1000; i++)
+  if (points_count > 0)
   {
-    bezier(control_points, 4, (float)i/(float)999);
+    for (i = 0; i < points_count; i++)
+    {
+      plotPoint((int)control_points[i][0], (int)control_points[i][1], 1.0);
+    }
+    if (points_count > 1)
+    {
+      for (i=0; i<RES; i++)
+      {
+        bezier(control_points, points_count, (float)i/(float)(RES-1));
+      }
+    }
   }
   glFlush();
 }
 
 void main(int argc, char **argv)
 {
-  printf("Starting ....\n");
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(DIMEN_X, DIMEN_Y);
 	glutCreateWindow("Bezier");
+  STATE = ADD_STATE;
 	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
+  glutKeyboardFunc(keyboard);
   init();
 	glutMainLoop();
 }
